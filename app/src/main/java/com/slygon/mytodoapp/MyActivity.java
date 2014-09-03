a642fa73f66b6e809ef35f369b17be80d01df194
Eli Goldberg <lgdragon28@gmail.com>
2014-09-04 00:49:56 +0300

    cleaned up code. getting json

--- app/src/main/java/com/slygon/mytodoapp/MyActivity.java
+++ app/src/main/java/com/slygon/mytodoapp/MyActivity.java
@@ -23,28 +23,42 @@ import org.apache.http.impl.client.*;
 import org.json.*;
 
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity
+{
 
     //    EditText etResponse;
-    TextView etResponse;
+    EditText etResponse;
     TextView tvIsConnected;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
         // get reference to the views
-        etResponse = (TextView) findViewById(R.id.etResponse);
+        etResponse = (EditText) findViewById(R.id.etResponse);
         tvIsConnected = (TextView) findViewById(R.id.tvIsConnected);
 
         // check if you are connected or not
-        if (isConnected()) {
+        if (isConnected())
+		{
             tvIsConnected.setBackgroundColor(0xFF00CC00);
             tvIsConnected.setText("You are conncted");
-        } else {
+        }
+		else
+		{
             tvIsConnected.setText("You are NOT conncted");
         }
+
+		try
+		{
+			new TodoClientUsage().getTodos(this);
+		}
+		catch (JSONException e)
+		{
+			Log.d("error", e.getLocalizedMessage());
+		}
 
         // call AsynTask to perform network operation on separate thread
 //		new HttpAsyncTask().execute("http://hmkcode.appspot.com/rest/controller/get.json");
@@ -53,64 +67,31 @@ public class MyActivity extends Activity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+	{
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+	{
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+		{
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    public static String GET(String url) {
-        InputStream inputStream = null;
-        String result = "";
-        try {
 
-            // create HttpClient
-            HttpClient httpclient = new DefaultHttpClient();
 
-            // make GET request to the given URL
-            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
-
-            // receive response as inputStream
-            inputStream = httpResponse.getEntity().getContent();
-
-            // convert inputstream to string
-            if (inputStream != null)
-                result = convertInputStreamToString(inputStream);
-            else
-                result = "Did not work!";
-
-        } catch (Exception e) {
-            Log.d("InputStream", e.getLocalizedMessage());
-        }
-
-        return result;
-    }
-
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while ((line = bufferedReader.readLine()) != null)
-            result += line;
-
-        inputStream.close();
-        return result;
-
-    }
-
-    public boolean isConnected() {
+    public boolean isConnected()
+	{
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected())
@@ -119,61 +100,58 @@ public class MyActivity extends Activity {
             return false;
     }
 
-    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... urls) {
-
-            return GET(urls[0]);
-        }
-
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
-            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
-
-            try {
-                //etResponse.setText(result);
-                JSONObject json = new JSONObject(result); // convert String to JSONObject
-
-                //JSONArray g = new JSONArray(result);
-                etResponse.setText(json.toString(1));
-
-
-//				JSONArray articles = json.getJSONArray("articleList"); // get articles array
-//				articles.length(); // --> 2
-//				articles.getJSONObject(0); // get first article in the array
-//				articles.getJSONObject(0).names(); // get first article keys [title,url,categories,tags]
-//				articles.getJSONObject(0).getString("url"); // return an article url
-            } catch (JSONException e) {
-                etResponse.setText(result);
-            }
-        }
-
-
-    }
 }
 
-class TodoClientUsage {
-    public void getPublicTimeline() throws JSONException {
-        TodoClient.get("statuses/public_timeline.json", null, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                // If the response is JSONObject instead of expected JSONArray
-            }
+class TodoClientUsage
+{
+    public void getTodos(MyActivity ac) throws JSONException
+	{
 
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
-                // Pull out the first event on the public timeline
-                try {
-                    JSONObject firstEvent = (JSONObject) timeline.get(0);
-                    String tweetText = firstEvent.getString("text");
+		final MyActivity a = ac;
+		TodoClient.get("api/todos", null, new JsonHttpResponseHandler() {
+				@Override
+				public void onSuccess(int statusCode, Header[] headers, JSONObject response)
+				{
+					// If the response is JSONObject instead of expected JSONArray
+					//Toast.makeText(a, "Got obj", Toast.LENGTH_LONG).show();
+				}
 
-                    // Do something with the response
-                    System.out.println(tweetText);
-                } catch (JSONException e) {
-                }
+				@Override
+				public void onSuccess(int statusCode, Header[] headers, JSONArray timeline)
+				{
+					// Pull out the first event on the public timeline
+					try
+					{
+						//Toast.makeText(a, "Got arr", Toast.LENGTH_LONG).show();
+						JSONObject firstEvent = (JSONObject) timeline.get(0);
+						//String tweetText = firstEvent.getString("text");
 
-            }
-        });
+						a.etResponse.setText(firstEvent.toString());
+
+						// Do something with the response
+						//System.out.println(tweetText);
+					}
+					catch (JSONException e)
+					{
+					}
+
+				}
+
+				@Override
+				public void onFailure(int statusCode, Header[] headers, JSONArray timeline)
+				{
+
+					Toast.makeText(a, "Error getting json", Toast.LENGTH_SHORT).show();
+				}
+
+				@Override
+				public void onStart()
+				{
+					Toast.makeText(a, "Retrieving todos", Toast.LENGTH_SHORT).show();
+				}
+
+			});
+
+
     }
 }
